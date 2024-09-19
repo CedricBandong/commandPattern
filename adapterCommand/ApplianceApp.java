63b004b34afccb6f7e3f8be766dbbdc916260ae4
Cedric Bandong <83577761+CedricBandong@users.noreply.github.com>
2024-09-19 16:46:01 +0800

    Create ApplianceApp.java

---
+++ adapterCommand/ApplianceApp.java
@@ -0,0 +1,26 @@
+package adapterPattern;
+
+public class ApplianceApp {
+
+    public static void main(String[] args){
+
+        Tv tv = new Tv();
+        RemoteControllerFunction tvRc = new TvAdapter(tv);
+
+        AirCon airCon = new AirCon();
+        RemoteControllerFunction airConRc = new AirconAdapter(airCon);
+
+        Speaker speaker = new Speaker();
+        RemoteControllerFunction speakerRc = new SpeakerAdapter(speaker);
+
+        System.out.println(tvRc.powerOn());
+        System.out.println(airConRc.powerOn());
+        System.out.println(speakerRc.powerOn());
+        System.out.println();
+
+        System.out.println(tvRc.pressPlusButton());
+        System.out.println(airConRc.pressPlusButton());
+        System.out.println(speakerRc.pressPlusButton());
+
+    }
+}
